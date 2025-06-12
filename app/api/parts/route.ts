44b3809732f2f2d91e38a62ef179841964daff23
CAR MECHANIC _ AUTOPOINT <carmechanic9351@gmail.com>
2025-06-12 06:50:52 +0000

    done

--- app/api/parts/route.ts
+++ app/api/parts/route.ts
@@ -81,6 +81,6 @@ import { NextResponse } from "next/server";
 //   }
 // }
 
-export async function GET(request: Request) {
+export async function GET() {
   return NextResponse.json({ success: "done" });
 }
